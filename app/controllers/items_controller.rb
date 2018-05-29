ce4bbf629c9d983607686f723f8f05a44e00dc89
Jimmy Pipkin <pipkinj@gmail.com>
2018-05-29 18:08:41 +0900

    added exchanges show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.user=current_user
+    @item.user = current_user
     if @item.save
       redirect_to item_path(@item)
     else
