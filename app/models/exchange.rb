48b79456768dc805962193dc2dd0ce2fd9ea1380
Edmond Akpan <Edmond.Akpan@gmail.com>
2018-05-29 19:37:25 +0900

    added validation to not borrow from self

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -6,7 +6,7 @@ class Exchange < ApplicationRecord
   validates :user, uniqueness: { scope: :item }
 
   validate :start_date_cannot_be_in_the_past, :end_date_cannot_be_before_start_date
-
+  validate :user_cannot_borrow_from_self
 
   def start_date_cannot_be_in_the_past
     if start_date.present? && start_date < Date.today
@@ -17,6 +17,12 @@ class Exchange < ApplicationRecord
   def end_date_cannot_be_before_start_date
     if end_date.present? && start_date.present? && end_date < start_date
       errors.add(:end_date, "can't be before Start date")
+    end
+  end
+
+  def user_cannot_borrow_from_self
+    if user == item.user
+      errors.add(:user, "cannot borrow from himself or herself")
     end
   end
 end
