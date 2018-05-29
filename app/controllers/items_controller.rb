d47de852c051c1c06e37a19c7b10ff5ba7a017a2
Nathan T Warne <nathan@nathantwarne.com>
2018-05-29 11:52:45 +0900

    item controller v 1.0

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,39 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :destroy]
+
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item
+    else
+      redirect_to :root
+    end
   end
 
   def show
+
   end
 
   def destroy
+    @item.destroy
+    redirect_to :root
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:id, :name, :description, :token_value, :photo, :user)
   end
 end
