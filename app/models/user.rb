42495220336315e60de720af2d44d4f313e20f92
Edmond Akpan <Edmond.Akpan@gmail.com>
2018-05-31 14:52:09 +0900

    clean

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  geocoded_by :location
   has_many :exchanges, dependent: :destroy
   has_many :items, dependent: :destroy
   has_many :lender_exchanges, through: :items, source: :exchange
 
   validates :username, presence: true, uniqueness: true
-  #validates :location
+  validates :location, presence: true
+  after_validation :geocode , if: :will_save_change_to_address?
 end
