03ef633f09c7818364347963b2a03c5fda86cbef
Nathan T Warne <nathan@nathantwarne.com>
2018-05-31 14:30:53 +0900

    updated seed file

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,3 +1,9 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+  def content_type_whitelist
+    /image\//
+end
+def extension_whitelist
+  %w(jpg jpeg gif png)
+end
 end
