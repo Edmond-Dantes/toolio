0d4994e46a5e9d4bef3b24e0116cd71c4dece603
Edmond Akpan <Edmond.Akpan@gmail.com>
2018-05-31 16:50:47 +0900

    autocomplete and map at the bottom

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,6 @@ class User < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :lender_exchanges, through: :items, source: :exchange
   validates :username, presence: true, uniqueness: true
-  #validates :location
+  validates :location, presence: true
+  after_validation :geocode, if: :will_save_change_to_location?
 end
