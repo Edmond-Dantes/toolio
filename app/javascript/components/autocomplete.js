174677228983a0449f7f5411ef80c74969b735fc
Edmond Akpan <Edmond.Akpan@gmail.com>
2018-06-02 16:40:05 +0900

    still trying for it

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -4,6 +4,7 @@ function autocomplete() {
 
     if (userLocation) {
       var autocomplete = new google.maps.places.Autocomplete(userLocation, { types: [ 'geocode' ] });
+      console.log(autocomplete)
       google.maps.event.addDomListener(userLocation, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault(); // Do not submit the form on Enter.
